5139c79cccefe03333834f1ee013a211de53a237
4tucode <4tucode@gmail.com>
2025-01-28 12:07:04 +0100

    version1

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    users: [], // Almacén para los usuarios
   },
   getters: {
   },
   mutations: {
+    SET_USERS(state, users) {
+      state.users = users; // Actualiza el estado con los usuarios obtenidos
+    },
   },
   actions: {
+    async obtener_usuarios({ commit }) {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!response.ok) {
+          throw new Error("Error al obtener los usuarios"); // Manejo de errores
+        }
+        const data = await response.json(); // Convierte la respuesta a JSON
+        commit("SET_USERS", data); // Llama a la mutación con los datos
+      } catch (error) {
+        console.error("Error en fetchUsers:", error);
+      }
+    },
   },
   modules: {
   }
